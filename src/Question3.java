73c55f53206f09da3e6179941b85038bd9b21ea4
kartikpruthi <kartik.pruthi@tothenew.com>
2023-03-29 16:39:38 +0530

    add

---
+++ src/Question3.java
@@ -0,0 +1,24 @@
+interface WorldCup {
+    default void myBody(){
+        System.out.println("Default is called by world cup");
+    }
+}
+interface Argentina extends WorldCup {
+    default void myBody(){
+        System.out.println("Default is called by argentina" );
+    }
+}
+interface Brazil extends WorldCup {
+    default void myBody(){
+        System.out.println("Default is called by brazil");
+    }
+}
+public class Question3 implements Argentina, Brazil {
+    public void myBody(){
+        System.out.println("Class method is called");
+    }
+    public static void main(String[] args) {
+        Question3 q = new Question3();
+        q.myBody();
+    }
+}
